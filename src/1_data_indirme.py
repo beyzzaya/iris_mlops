ce231a780fdcf931fe2001f45a82ec47644a74b5
Beyza Nur Yaylaoğlu <beyzanur5702@hotmail.com>
2025-09-24 10:42:45 +0300

    data indirme .py dosyası oluşturuldu.

---
+++ src/1_data_indirme.py
@@ -0,0 +1,20 @@
+from sklearn.datasets import load_iris
+import os 
+
+
+def main():
+    print("------ 1.Veri İndirme ------")
+
+    os.makedirs("data/raw", exits_ok=True)
+
+    iris = load_iris(as_frame=True)
+    iris = iris.frame
+    iris.to_csv("data/raw/iris_raw.csv", index=False)
+
+    print(f"iris dataset's shape: {iris.shape}")
+    print(f"iris datasetinde  {iris.isna().sum().sum()} boş değer var.")
+    print(f"iris datasetinin  columnları: {iris.columns}")
+
+
+if __name__ == "__main__":
+    main()
